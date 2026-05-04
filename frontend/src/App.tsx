dbd7d175597feea7ac596cd41dbe8989a082e819
Baraa Youssry <baraayoussry54@gmail.com>
2026-05-05 01:27:24 +0300

    يارب

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Navbar />
       <Hero />
       <WhyChooseHICIT />
-      {/* <Departments /> */}
+      <Departments />
       <LMSSection />
       <DepartmentsGrid />
       <ExperienceSection />
